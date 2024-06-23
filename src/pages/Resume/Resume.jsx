60cd0de9d6923c969c60f9e1cb60e6cc2d04c20b
CJ <chasanid@gmail.com>
2024-06-23 00:45:15 -0400

    adding resume download link

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import workHistory from './workHistory.js';
 import Job from '../../components/Job/Job.jsx';
+import './Resume.scss'
 
 const Resume = () => {
   return (
     <div>
-      <h1>📄Resume Page</h1>
+      <h1>📄Resume Page<a className="download-link" href="/Chasani_Resume.pdf">Download <i className='bx bxs-download' ></i></a></h1>
       <p>
         Well-rounded Fullstack Engineer with over 12 years of experience building customer-facing and internal tools. Primarily
         focused in React accompanied by strong professional backend experience in backend languages such as Ruby on Rails and
