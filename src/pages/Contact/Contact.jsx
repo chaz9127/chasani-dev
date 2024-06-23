f614c66d063e26918fd2f6684360ddc2a0537876
CJ <chasanid@gmail.com>
2024-06-23 19:48:09 -0400

    wip

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -17,38 +17,41 @@ const Contact = () => {
 
   return (
     <form action="https://formsubmit.co/el/xikona" method="POST" className="contact-form">
-      <div className="form-group">
-        <label htmlFor="fullName">Full Name</label>
-        <input 
-          type="text" 
-          id="fullName" 
-          name="fullName" 
-          value={formData.fullName} 
-          onChange={handleChange} 
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Return Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          value={formData.email} 
-          onChange={handleChange} 
-          required 
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="message">Message</label>
-        <textarea 
-          id="message" 
-          name="message" 
-          value={formData.message} 
-          onChange={handleChange} 
-          required 
-        ></textarea>
-      </div>
-      <button type="submit">Send</button>
+        <div className="form-group">
+            <label htmlFor="fullName">Full Name</label>
+            <input 
+                type="text" 
+                id="fullName" 
+                name="fullName" 
+                value={formData.fullName} 
+                onChange={handleChange} 
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="email">Return Email</label>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                value={formData.email} 
+                onChange={handleChange} 
+                required 
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea 
+                id="message" 
+                name="message" 
+                value={formData.message} 
+                onChange={handleChange} 
+                required 
+            ></textarea>
+        </div>
+        <button type="submit">Send</button>
+
+        <input type="hidden" name="_subject" value={`Message from TheMCU.FYI`} />
+        <input type="hidden" name="_captcha" value="false" />
     </form>
   );
 };
